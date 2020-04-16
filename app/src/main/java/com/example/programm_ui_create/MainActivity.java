69cb2201adaf1d5d9c9bebfb91cac0914908cd9e
savelgmx <savel@gmx.com>
2020-04-16 17:37:10 +0700

    коммит 4

--- app/src/main/java/com/example/programm_ui_create/MainActivity.java
+++ app/src/main/java/com/example/programm_ui_create/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.programm_ui_create;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,29 +29,29 @@ public class MainActivity extends AppCompatActivity {
         RelativeLayout relativeLayout = new RelativeLayout(this); //вложена в корневую
         LinearLayout linearLayout = new LinearLayout(this); //2-й уровень вложенности
         //задать параметры
- /*       xmlns:tools="http://schemas.android.com/tools"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:padding="16dp"
-*/
-       FrameLayout.LayoutParams fParams = new FrameLayout.LayoutParams(frameLayoutMatchParent,frameLayoutMatchParent);
+        FrameLayout.LayoutParams fParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT);
+
+ //       setContentView(frameLayout);
+
         frameLayout.addView(relativeLayout, fParams); //add inside  layout
-/*
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_gravity="center"
-
-*/
-
         RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(relativeLayoutWrapContent,relativeLayoutWrapContent);
         relativeLayout.setGravity(Gravity.CENTER);
 
 
 
-         //добавить image view
+        //добавить image view
+        ImageView imageView = new ImageView(this);
+        imageView.setImageDrawable(Drawable.createFromPath("@drawable/bruce_lee"));
+        relativeLayout.addView(imageView);
+
+
+
         //add text view
         TextView textView = new TextView(this);
+
         textView.setText("TextView");
+
+
         //textView.setLayoutParams(lpView);
         relativeLayout.addView(textView);
 
@@ -67,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         //LayoutParams(int width, int height)
         LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
                 linearLayourWrapContent, linearLayourWrapContent);
+
+        linearLayout.addView(textView);
 
         linearLayout.addView(btnPrevious,lParams);
         linearLayout.addView(btnNext,lParams);
